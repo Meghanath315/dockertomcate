7a353c4f5044eb602c9716f1e7f1cc00643d4aa5
meghanath315 <meghanath55555@gmail.com>
2020-05-05 12:11:47 +0530

    hi

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS exmple') {
     scm {
-             git('git://github.com/Meghanath315/nodejs.git') { node -> // is hudson.pligins.git.GitSCM
+             git('git://github.com/Meghanath315/dockertomcate.git') { node -> // is hudson.pligins.git.GitSCM
                  node / gitConfigName('DSL User')
                  node / gitConfigEmail('1804megh@gmail.com')
                  }
--- nodejsdocker.groovy
+++ nodejsdocker.groovy
@@ -1,19 +1,19 @@
 job('NodeJS Docker exmple') {
     scm {
              git('git://github.com/Meghanath315/dockertomcate.git') { node -> // is hudson.pligins.git.GitSCM
-                 node / gitConfigName('DSL User')
-                 node / gitConfigEmail('1804megh@gmail.com')
-                 }
+             node / gitConfigName('DSL User')
+             node / gitConfigEmail('1804megh@gmail.com')
         }
-        triggers {
+    }
+    triggers {
         scm('H/5 * * * *')
     }
-        wrappers {
-       nodejs('nodejs') // this is the namme of NodeJS installation in
+    wrappers {
+        nodejs('nodejs') // this is the namme of NodeJS installation in
                         // Manage Jenkins -> NodeJS Installations -> Name
-        }
-        steps {
-            dockerBuildAndPublish{
+    }
+    steps {
+        dockerBuildAndPublish {
 	        repositoryName('meghanath/nodejs-dsl')
 		tag('${GIT_REVISION, length=9}')
 		registryCredentials('dockerhub')
@@ -21,7 +21,7 @@ job('NodeJS Docker exmple') {
 		forceTag(false)
 		createFingerprints(false)
 		skipDecorate()
-                }
+    }
 }
 }
 
